3371a112d9ce286c21f5fdf2ca649b560be65127
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-03 12:36:40 +0600

    create a file for config firebase

---
+++ src/fireabase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+   apiKey: "AIzaSyBj9F3dv6pzatV-jiM8Jn5mwaIu8gvw8iM",
+   authDomain: "challange-e785b.firebaseapp.com",
+   projectId: "challange-e785b",
+   storageBucket: "challange-e785b.appspot.com",
+   messagingSenderId: "546457793357",
+   appId: "1:546457793357:web:c15764bfbd783e062ffd04",
+   measurementId: "G-WYQXJ2587L"
+};
