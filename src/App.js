0a71b4a4b52d0fdad808f21f67246756cbd07a89
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-03 12:41:43 +0600

    remove css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
    return (
       <div className="App">
-
+         <h2>Hello This is Naim Biswas</h2>
+         <h3>Let's Build a Amazon Clone</h3>
       </div>
    );
 }
