6e59b0fe34f47022d0029d3bddd8a5497c50a4b6
NaimBiswas <nayeembiswas2@gmail.com>
2021-02-03 13:30:48 +0600

    static header done

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,53 @@ import Logo from './Logo.svg'
 const Header = () => {
    return (
       <div className='header'>
+         {/* logo  */}
          <img className='header_logo' src={Logo} alt="" />
+         {/* search  */}
+         <div className="header_search">
+            <input type="text" className="header_seacrhIn" />
+         </div>
+         {/* header nav bar  */}
+         <div className="header_nav">
+            {/* right side bar option  */}
+            <div className="header_option">
+               <span className="header_optionOneTop">
+                  Hello Guest
+                </span>
+               <span className="header_optionOneBottom">
+                  Sing In
+               </span>
+            </div>
+
+            <div className="header_option">
+               <span className="header_optionTwoTop">
+                  Return's
+                </span>
+               <span className="header_optionTwoBottom">
+                  & Orders
+               </span>
+            </div>
+
+            <div className="header_option">
+               <span className="header_optionThreeTop">
+                  Your
+                </span>
+               <span className="header_optionThreeBottom">
+                  Prime
+               </span>
+            </div>
+
+            <div className="header_option">
+               <span className="header_optionFourTop">
+                  Hello Guest
+                </span>
+               <span className="header_optionFourBottom">
+                  Sing In
+               </span>
+            </div>
+
+
+         </div>
       </div>
    )
 }
