3ef3f15a273092e11881acf2d81f28d1da518b3c
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-01 22:05:55 +0600

    set cart page two section"

--- src/Components/CartPage/Cart.js
+++ src/Components/CartPage/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react'
-
+import './Cart.css'
 const Cart = () => {
    return (
-      <div>
-         <h3>Hello I am Card</h3>
-      </div>
+      <>
+         {/* Creackout section   */}
+         <section>
+            <div className="container-fluid">
+               <div className="container">
+                  <div className="row">
+                     <div className="col-lg-6">
+                        <div className=" checkoutLeft">
+
+                        </div>
+                     </div>
+                     <div className="col-lg-6">
+                        <div className=" checkoutRight" >
+
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </>
    )
 }
 
