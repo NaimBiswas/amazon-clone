93da38485f767d355cc3c3a94f2fde04fcfb7968
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-01 22:30:08 +0600

    crate one cart product

--- src/Components/CartPage/Cart.js
+++ src/Components/CartPage/Cart.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 import './Cart.css'
+
+
 const Cart = () => {
    return (
       <>
@@ -16,6 +19,22 @@ const Cart = () => {
                            <div className="basket">
                               <h2 className='mt-5 basketHeader'>Your Shopping Basket</h2>
                               <hr />
+                              <div className="row">
+                                 <div className="col-lg-5">
+                                    <div className="cartImage">
+                                       <img
+                                          style={{ height: '400px' }}
+                                          className="img-fluid w-100"
+                                          src="https://m.media-amazon.com/images/I/61id45RQjoL._AC_UY218_.jpg" alt="" />
+                                    </div>
+                                 </div>
+                                 <div className="col-lg-7">
+                                    <h2>Title</h2>
+                                    <p>Rating</p>
+                                    <p>Price</p>
+                                    <Button variant="warning">Remove from Basket</Button>
+                                 </div>
+                              </div>
                            </div>
                         </div>
                      </div>
