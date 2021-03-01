42096fc3a92775fe3d4dd806c867dede3168b288
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-01 11:27:08 +0600

    Create product alyout

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,30 @@
 
 import React from 'react'
+import { Fragment } from 'react'
 import Slider from './Slider/Slider'
 
 const Home = () => {
    return (
-      <div>
+      <Fragment>
+         {/* HOme SLider serction  */}
          <Slider />
-      </div>
+         <section>
+            <div className="home-row-one ">
+               <div className="col-lg-6"></div>
+               <div className="col-lg-6"></div>
+            </div>
+            <div className="home-row-tow">
+               <div className="col-lg-4"></div>
+               <div className="col-lg-4"></div>
+               <div className="col-lg-4"></div>
+            </div>
+            <div className="home-row-three">
+               <div className="col-lg-12">
+
+               </div>
+            </div>
+         </section>
+      </Fragment>
    )
 }
 
