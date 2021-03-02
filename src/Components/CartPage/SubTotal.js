5b16d8a1f5aca27a38b337094e3e67df7d8590d1
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-02 13:24:33 +0600

    successfully calculate total cart added price

--- src/Components/CartPage/SubTotal.js
+++ src/Components/CartPage/SubTotal.js
@@ -4,13 +4,17 @@ import { useStateValue } from '../../StateProvider'
 
 const SubTotal = () => {
    const [{ basket },] = useStateValue()
+   var total = 0;
+   basket.map(itm => (
+      total += parseFloat(itm.price)
+   ))
 
 
 
    return (
       <>
          <div className="Card">
-            <h3>Subtotal ({basket.length} item) : $ 10</h3>
+            <h3>Subtotal ({basket.length} item) : $ {total}</h3>
             <input type="checkbox" /><span className='ml-2'>This order contain a gif</span><br />
             <Button className='d-block w-100 mt-4 ' variant='warning'>Proceed to Checkout</Button>
          </div>
