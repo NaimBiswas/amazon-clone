c05ad70e97f03c9e6adc63f7fae0ae7d04cc2a90
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-03 23:35:42 +0600

    create forntend login view

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,11 +1,64 @@
 import React from 'react'
 import { Fragment } from 'react'
+import { Button } from 'react-bootstrap'
+import './Login.css'
 
 const Login = () => {
+   const SignIn = (e) => {
+      e.preventDefault()
+   }
    return (
       <Fragment>
-         <h3>I am Login Component</h3>
-      </Fragment>
+         <form action="">
+
+
+            <div className="loginpage">
+               <h2 className='text-center'>
+                  <img src="http://localhost:3000/static/media/Logo.4a378fe4.svg" alt="" />
+               </h2>
+
+
+               <div className="form border col-lg-6">
+                  <h2 className='mt-4 '>Sign In</h2>
+                  <div className="form-group row mt-4 ">
+                     <div className="form-left col-lg-3 ">
+                        <h5 className=''>Email:👉</h5>
+                     </div>
+                     <div className="form-right col-lg-9">
+                        <input required type="email" className='form-control' />
+                     </div>
+                  </div>
+
+                  <div className="form-group row ">
+                     <div className="form-left col-lg-3">
+                        <h5>Password:👉</h5>
+                     </div>
+                     <div className="form-right col-lg-9">
+                        <input required type="password" className='form-control' />
+                     </div>
+                  </div>
+
+                  <div className="form-group row ">
+                     <Button onClick={SignIn} type='submit' variant='warning' className='d-block w-100 mr-3 ml-3  mb-2 mt-3'>Sign In</Button>
+                  </div>
+                  <div className="form-group row  ">
+                     <p className='ml-3 mr-3'>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi suscipit deserunt dolore consectetur harum eius, ipsa soluta corporis error, ipsum facere veniam sunt sequi itaque reprehenderit labore similique, dicta doloremque?
+                    </p>
+                  </div>
+
+                  <div className="form-group row ">
+                     <Button variant='outline-warning' className='d-block w-100 mr-3 ml-3 mb-3 createAccount'>Create An Account</Button>
+                  </div>
+
+               </div>
+
+
+
+            </div>
+
+         </form>
+      </Fragment >
    )
 }
 
