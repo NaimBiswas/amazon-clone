e251d6a5d5a940b3d1ae6056b0a40c80f69a744f
NaimBiswas <nayeembiswas2@gmail.com>
2021-03-05 10:52:16 +0600

    Add a loading spinner

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { StateProvider } from './StateProvider';
 import reducer, { initialState } from './reducer';
+import { Spinner } from 'react-bootstrap';
 const App = lazy(() => import('./App'))
 
 
@@ -15,7 +16,11 @@ ReactDOM.render(
          <div
             style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}
          >
-            <h2 className='text-info'>Loading...</h2>
+            <Spinner animation="border" variant='info' role="status">
+               <span className="sr-only">Loading...</span>
+            </Spinner>
+            <h2 className='text-info mt-2'>
+               Loading...</h2>
          </div>}>
          <StateProvider initialState={initialState} reducer={reducer}>
             <App />
